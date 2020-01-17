2ef361194b95832778182e3f2fa1e6e882041056
“Zach <z_wood@baylor.edu>
2020-01-17 22:04:13 +0000

    array disp one char per line

--- Arrays.rb
+++ Arrays.rb
@@ -1,2 +1,17 @@
-ary = [1, "two", 3.0]
-puts ary
+class Image
+def initialize 
+  # insert array and variables
+  @ary = [[0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0]]
+end
+
+def output_image
+  puts @ary
+end
+
+
+end
+image = Image.new
+image.output_image
